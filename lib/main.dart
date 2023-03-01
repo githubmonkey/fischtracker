3c4b3001597b7a3c8a65312dd0c4338925b0dcae
Sylvia <sylvie@gmail.com>
2023-03-01 22:29:18 +0200

    configure crashlytics using game_template example

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 //import 'package:auth_widget_builder/auth_widget_builder.dart';
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:fischtracker/crashlytics/crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logging/logging.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:fischtracker/firebase_options.dart';
 import 'package:fischtracker/src/app.dart';
@@ -11,17 +16,47 @@ import 'package:fischtracker/src/features/onboarding/data/onboarding_repository.
 // ignore:depend_on_referenced_packages
 import 'package:flutter_web_plugins/url_strategy.dart';
 
+
+// Toggle this for testing Crashlytics in your app locally.
+const _kTestingCrashlytics = true;
+
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+  FirebaseCrashlytics? crashlytics;
+  if (!kIsWeb && (Platform.isIOS || Platform.isAndroid)) {
+    crashlytics = _kTestingCrashlytics ? FirebaseCrashlytics.instance : null;
+  }
+
+  await guardWithCrashlytics(
+    guardedMain,
+    crashlytics: crashlytics,
   );
+}
+
+/// Without logging and crash reporting, this would be `void main()`.
+void guardedMain() async {
+  if (kReleaseMode) {
+    // Don't log anything below warnings in production.
+    Logger.root.level = Level.WARNING;
+  }
+  Logger.root.onRecord.listen((record) {
+    debugPrint('${record.level.name}: ${record.time}: '
+        '${record.loggerName}: '
+        '${record.message}');
+  });
+
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    debugPrint("Firebase couldn't be initialized: $e");
+  }
+
   // turn off the # in the URLs on the web
   usePathUrlStrategy();
   final sharedPreferences = await SharedPreferences.getInstance();
-  // * Register error handlers. For more info, see:
-  // * https://docs.flutter.dev/testing/errors
-  registerErrorHandlers();
+
   // * Entry point of the app
 
   final container = ProviderContainer(
