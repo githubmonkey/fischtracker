3e500b3241f62f20190de3b471aabcf6b509213a
Sylvia <sylvie@gmail.com>
2023-03-28 22:40:33 +0200

    replace entry route with push

--- lib/src/features/jobs/presentation/job_entries_screen/job_entries_list.dart
+++ lib/src/features/jobs/presentation/job_entries_screen/job_entries_list.dart
@@ -33,7 +33,7 @@ class JobEntriesList extends ConsumerWidget {
               .read(jobsEntriesListControllerProvider.notifier)
               .deleteEntry(entry.id),
           onTap: () => context.goNamed(
-            AppRoute.entryviajob.name,
+            AppRoute.entryViaJob.name,
             params: {'cid': job.catId, 'jid': job.id, 'eid': entry.id},
             extra: entry,
           ),
