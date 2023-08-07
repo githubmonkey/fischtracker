5806ac259649c72577eb12bfc1a12fc2f04174ac
Sylvia <sylvie@gmail.com>
2023-08-07 22:54:17 +0200

    Update all libraries and regenerate provider code

--- lib/src/features/jobs/data/jobs_repository.g.dart
+++ lib/src/features/jobs/data/jobs_repository.g.dart
@@ -152,4 +152,5 @@ final jobsStreamProvider = AutoDisposeStreamProvider<List<Job>>.internal(
 );
 
 typedef JobsStreamRef = AutoDisposeStreamProviderRef<List<Job>>;
-// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
